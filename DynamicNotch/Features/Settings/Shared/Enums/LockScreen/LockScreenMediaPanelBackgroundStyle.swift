8a8722b6389895ddd0aec7c0797ed9b97847638c
Evgeniy Petrukovich <evgeniy.petrukovich@icloud.com>
2026-08-21 23:38:06 +0300

    Restore animated lock-screen media panel background style and reorganize notifications

--- DynamicNotch/Features/Settings/Shared/Enums/LockScreen/LockScreenMediaPanelBackgroundStyle.swift
+++ DynamicNotch/Features/Settings/Shared/Enums/LockScreen/LockScreenMediaPanelBackgroundStyle.swift
@@ -1,11 +1,14 @@
 import SwiftUI
 
 enum LockScreenMediaPanelBackgroundStyle: String, CaseIterable {
+    case animatedArtwork
     case staticArtwork
     case black
 
     var title: LocalizedStringKey {
         switch self {
+        case .animatedArtwork:
+            return "settings.lockScreen.mediaPanelBackgroundStyle.animatedArtwork"
         case .staticArtwork:
             return "settings.lockScreen.mediaPanelBackgroundStyle.staticArtwork"
         case .black:
